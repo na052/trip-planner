e233f05872a084eaafcb1dc69b8aaae4ee687863
Naoki Ando <nao0502oan@akane.waseda.jp>
2026-09-06 14:49:25 +0900

    Google Mapをアプリに表示させる機能を搭載

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,29 @@
-import Image from "next/image";
+"use client";
+
+import { APIProvider, Map } from "@vis.gl/react-google-maps";
 
 export default function Home() {
+  const apiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
+
+  if (!apiKey) {
+    return <p>Google Maps APIキーが設定されていません。</p>;
+  }
+
   return (
     <main>
-      <h1> Trip Planner </h1>
-      <p>Welcome to the Trip Planner!</p>
+      <h1>Trip Planner</h1>
+
+      <div style={{ width: "100%", height: "600px" }}>
+        <APIProvider apiKey={apiKey}>
+          <Map
+            defaultCenter={{
+              lat: 38.681236,
+              lng: 139.767125,
+            }}
+            defaultZoom={8}
+          />
+        </APIProvider>
+      </div>
     </main>
   );
 }
