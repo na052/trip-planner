3d488febc4b029dc57aef9f9e6f9a1787f0af7e5
Naoki Ando <nao0502oan@akane.waseda.jp>
2026-09-06 16:24:02 +0900

    マップにピンを立てられるようになった

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,35 @@
 "use client";
 
-import { APIProvider, Map } from "@vis.gl/react-google-maps";
+
+import {useState} from "react";
+import { APIProvider,
+  Map,
+  AdvancedMarker,
+  MapMouseEvent } from "@vis.gl/react-google-maps";
+
+type Position = {
+  lat: number;
+  lng: number;
+};
 
 export default function Home() {
   const apiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
 
+  const [position, setPosition] = useState<Position | null>(null);
+ 
   if (!apiKey) {
     return <p>Google Maps APIキーが設定されていません。</p>;
   }
+
+  const handleMapClick = (event: MapMouseEvent) => {
+    if(!event.detail.latLng){
+      return;
+    }
+    setPosition({
+      lat: event.detail.latLng.lat,
+      lng: event.detail.latLng.lng
+    });
+  };
 
   return (
     <main>
@@ -17,11 +39,16 @@ export default function Home() {
         <APIProvider apiKey={apiKey}>
           <Map
             defaultCenter={{
-              lat: 38.681236,
+              lat: 35.681236,
               lng: 139.767125,
             }}
             defaultZoom={8}
-          />
+            onClick={handleMapClick}
+            mapId= "DEMO_MAP_ID"
+          >
+            {position && <AdvancedMarker position={position} />}
+
+          </Map>
         </APIProvider>
       </div>
     </main>
