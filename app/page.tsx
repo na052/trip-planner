34f1ae7148ab3c1befa922681e97319677402365
Naoki Ando <nao0502oan@akane.waseda.jp>
2026-09-06 14:13:12 +0900

    next.jsをインストールしてからのプロジェクトページを開けるように設定

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <h1> Trip Planner </h1>
+      <p>Welcome to the Trip Planner!</p>
+    </main>
+  );
+}
